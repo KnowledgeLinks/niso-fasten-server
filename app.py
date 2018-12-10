9a5ea2de4c025940a3519724f8e89415d4050a91
Jeremy Nelson <jermnelson@gmail.com>
2018-12-10 06:47:02 -0700

    Adding placeholder for major func

--- app.py
+++ app.py
@@ -3,11 +3,45 @@ __version__ = "0.0.1"
 
 import urllib.parse
 import lxml.etree as etree
+import click
 import responder
 
-NAMESPACES = {"lcf": "http://ns.bic.org.uk/lcf/1.0"}
+NAMESPACES = {"ncip": "http://www.niso.org/2008/ncip"}
 
 api = responder.API()
+
+@api.route("/item/{message}")
+async def request_item(req, resp, message=None):
+    if message is None:
+        resp.status_code = api.status_codes.HTTP_400
+        resp.text = "Missing message"
+    raw_request = await req.content
+    try:
+        message_xml = etree.XML(raw_request)
+    except etree.XMLSyntaxError:
+        raw_queries = await req.content
+        queries = urllib.parse.parse_qs(raw_queries)    
+        if message.startswith("RequestItem"):
+            # Returns RequestItemResponse
+            message_xml = api.template("RequestItem.xml", **queries)
+        elif message.startswith("CancelRequestItem"):
+            # Returns Success or Failure
+            message_xml = api.template("CancelRequestItem.xml", **queries)
+        elif message.startswith("CheckOutItem"):
+            # Returns CheckOutItemResponse or ItemCheckoutOut message
+            message_xml = api.template("CheckOutItemResponse.xml")
+        elif message.startswith("CheckInItem"):
+            # Returns Success or Failure
+            pass
+        elif message.startswith("RenewItem"):
+            # Returns Success or Failure
+            pass
+        elif message.startswith("LookupItem"):
+            # Returns LookupItem Response
+            pass
+        else:
+            resp.status_code = api.status_codes.HTTP_400
+            resp.text = f"Unknown {message}"
 
 @api.route("/loans/")
 @api.route("/loans/{identifier}")
